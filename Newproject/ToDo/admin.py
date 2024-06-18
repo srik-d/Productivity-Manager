96c7d06f6698999909f47ca10cf28bf0e77e6eae
SriKvya <112415181+srik-d@users.noreply.github.com>
2024-06-18 12:26:22 +0530

    Add files via upload
    Modified admin.py , restricted superuser's access to see all tasks created since it is a privacy issue . since only the user that created respective tasks should have access.

--- Newproject/ToDo/admin.py
+++ Newproject/ToDo/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 from ToDo.models import Task
-# Register your models here.
-admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'complete', 'create')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.filter(user=request.user)
+
+admin.site.register(Task, TaskAdmin)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
